4c7f3cd9eeec6f106bced72c3f03160c266b1f32
AbdulMunnam07 <createrking40@gmail.com>
2024-08-01 00:25:13 +0500

    Check

---
+++ ES6 Basic/Ternary Operator/index.js
@@ -0,0 +1,21 @@
+// Ternary op
+
+// Ternary operator is used in place of if-else condition
+
+if (11>6) {
+    console.log("Correct");
+}
+else{
+    console.log("wrong");
+}
+
+
+// (Condition)? True:False
+
+
+(11 > 6)? console.log("Correct"):console.log("Wrong");
+
+let a = 11
+let b = 9
+
+(a > b)? console.log("True"):console.log("false");
